7134d4189f04c9d9631cd5079b5b837c2025b9c0
Aislinn Hayes <aislinn.m.b.hayes@gmail.com>
2025-06-09 08:54:02 +0100

    show handlers don't work with only cy.mount

--- src/stories/AppUsingRunMethod.cy.tsx
+++ src/stories/AppUsingRunMethod.cy.tsx
@@ -9,5 +9,6 @@ describe('Test on app-level story using MSW and run method', () => {
         void AppStory.run(); // if we use this, then the DOM gets mounted twice. once in data-cy-root and maybe another below by decorators?
         cy.mount(<AppStory />);
         cy.findByRole('heading', { level: 1, name: 'Vite + React' }).should('be.visible');
+        cy.findByText('A mocked response!').should('be.visible');
     });
 });
